8e5e6027bf6ec80ede7a6e28bdba6690eaac36ed
AT Will <at_soamazed@icloud.com>
2025-04-13 19:40:47 +0000

    Getting text from logged

--- login.py
+++ login.py
@@ -15,16 +15,25 @@ def get_driver():
   driver.get("https://automated.pythonanywhere.com/login/")
   return driver
 
+def clean_text(text):
+  output = float(text.split(": ")[1])
+  return output
 
 def main():
   driver = get_driver()
   time.sleep(2)
+  
   element = driver.find_element(by="id", value="id_username").send_keys("automated")
   time.sleep(2)
-  element = driver.find_element(by="id", value="id_password").send_keys("automatedautomated" + Keys.RETURN)
-  driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
-  print(driver.current_url)
   
+  element = driver.find_element(by="id", value="id_password").send_keys("automatedautomated" + Keys.RETURN)
+  time.sleep(2)
+  
+  driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
+  time.sleep(2)
+
+  text = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]").text
+  return clean_text(text)
 
 
 print(main())
